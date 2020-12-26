b9b1ab3bdbac4113ffe7e503833e27abc78b2c29
choppaseksu <76611507+choppaseksu@users.noreply.github.com>
2020-12-26 12:23:00 +0300

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,42 @@
+let config = {
+
+  // Задержка загрузки (1 секунда)
+  preloaderDelay: 1,
+
+  // Задержка появления видео (4.2 секунды)
+  showDelay: 1.5, 
+
+  // Заголовок
+  title: 'choppaseksu',
+
+  // Социальные сети
+  social: {
+    telegram: '@choppaseksu',
+    steam: '76561198066089982',
+    vk: 'choppaseksu', 
+  },
+
+
+  // Цвет иконки загрузки
+  preloaderColor: '#7B68EE',
+
+
+  // Цвет кнопки play
+  playButtonColor: '#00BFFF',
+  // Цвет тени кнопки play
+  playButtonShadowColor: 'rgba(25, 25, 112, 0.6)',
+
+
+  // Цвет заголовка
+  titleColor: '#000000',
+  // Цвет тени загаловка
+  titleShadowColor: 'rgba(255, 0, 55, 0.6)',
+
+
+ 
+
+  // Цвет иконок социальных сетей
+  socialColor: '#000000',
+  // Цвет тени иконок социальных сетей
+  socialShadowColor: 'rgba(255, 255, 255, 0.2)',
+}
